d1f3f0cfdbc22d9a723ce3bb43b43726c5f003fb
Joel Okoro <jukachi95@gmail.com>
2020-03-11 22:53:21 +0000

    Updated appendPageLinks function
    Added working loop which now gives one link at a time, the class active.

--- js/script.js
+++ js/script.js
@@ -6,16 +6,9 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+
+
+// Global variables that store DOM Elements to reference
 
 // Create variable which will store all of the list items
 let listItems = document.querySelectorAll('.student-item');
@@ -23,9 +16,6 @@ let listItems = document.querySelectorAll('.student-item');
 // Create a variable which will store the amount of students that will be displayed
 let itemsPerPage = 10;
 
-// Testing 
-// console.log(listItems)
-// console.log(itemsPerPage)
 
 
 
@@ -44,84 +34,110 @@ let itemsPerPage = 10;
        "invoke" the function 
 ***/
 
+// showPage function to show ten students
+
 function showPage(list, page){
 let startIndex = (page * itemsPerPage) - itemsPerPage ;
 let lastIndex = (page * itemsPerPage) - 1;
 
-      for(i = 0; i < list.length; i++){
+            for(i = 0; i < list.length; i++){
 
-            if(i >= startIndex && i <= lastIndex){
-                list[i].style.display = ''
-            } else{
-               list[i].style.display = 'none'
+               // If the indexis greater than zero and less than the last item, display
+                  if(i >= startIndex && i <= lastIndex){
+
+                     list[i].style.display = ''
+
+
+                  } else{
+
+                     // Else, do not show by giving it a display of none
+                     list[i].style.display = 'none'
+
+                  }
             }
-            
-         
-      }
 }
 
 
 
 
+// appendPageLinks to generate, append and add functionaility to pagination links
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+// List parameter which the variable listItems will be passed as a parameter
 function appendPageLinks(list){
 
-        // Determine how many pages are needed by dividing the total number of items by the max number of items per page
-      const neededPages = list.length / itemsPerPage;
+            // Determine how many pages are needed by dividing the total number of items by the max number of items per page
+            // Initial value is 5.4, so used Math.ceil to change it to 6
+            const neededPages = Math.ceil(list.length / itemsPerPage);
 
-    // Create Div to store pagination links
-    const div = document.createElement("div");
-    div.className = 'pagination';
 
-   // Create ul elements to append <a> links to
-   const ul = document.createElement("ul");
 
-   // Append ul to the div
-   div.appendChild(ul);
+            // Create div element to store pagination links
+            const div = document.createElement("div");
 
-   // Get hold of parent div that newly created div will be appended to
-   let parentDiv = document.querySelector(".page");
-   parentDiv.appendChild(div);
-  
-   // Set variable for <a></a> links
-   let links;
-   // Make first element child of ul active
-   // Make ul children loopable
+
+            //  Give the div element a className of pagination in order for it to be given css style
+            div.className = 'pagination';
+
+
+            // Create ul elements that links will be appended to
+            const ul = document.createElement("ul");
+
+
+            // Append the newly created ul to the div with class of pagination
+            div.appendChild(ul);
+
+
+            // Get hold of the parent div that the newly created div will be appended to
+            let parentDiv = document.querySelector(".page");
+            parentDiv.appendChild(div);
+
+         
+            // Declare variable for <a></a> tags
+            let links;
+
    
-         for(i = 1; i < neededPages; i++){
-            
-                  links = document.createElement("a");
-                  let li = document.createElement('li');
-                  links.href = '#';
-                  links.textContent = i;
-                  li.appendChild(links)
-                  ul.appendChild(li)
-                   let listChildren = li.children[0]
-                  console.log(listChildren)
+                  for(i = 1; i <= neededPages; i++){
+                     
+                          // Create <a></a> elements
+                           links = document.createElement("a");
 
+                          // Create li element
+                           let li = document.createElement('li');
 
-                              li.addEventListener('click', (e)=>{
+                           // Give <a></a> elements href value of '#'
+                           links.href = '#';
 
-                                 let activeClass = e.target;     
-                                 activeClass.className = 'active';  
-                                       showPage(list,e.target.textContent)
-                                 
-                              })
+                           // Give the <a></a> elements the textContent of the current iteration of the loop
+                           links.textContent = i;
 
+                           // Append the <a></a> elements variable (links) to the li
+                           li.appendChild(links)
 
+                           // The li will then be appened to the ul tht was created outside of the current for-loop
+                           ul.appendChild(li)
+                           
 
-         }
+                                    // Add event listener to <a></a> tags
+                                       links.addEventListener('click', (e)=>{
+
+                                          const pageLinks = document.querySelectorAll('.pagination a')
+                                          console.log(pageLinks)
+                                          for(let i =0; i < pageLinks.length; i++){
+                                          pageLinks[i].className = ''
+                                          }
+                                          let activeClass = e.target;     
+                                          activeClass.className = 'active';  
+                                                showPage(list,e.target.textContent)
+                                          
+                                       })
+
+                  }
 
   
    
 }
 
-
+// Call the functions with appropriate values
 showPage(listItems, 1)
 appendPageLinks(listItems)
 
